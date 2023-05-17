44eeb77533c65cc4b4adf73a1627fe2a2e3208a5
yahyaqr <aqrom.yahya75@gmail.com>
2023-05-18 04:41:03 +0700

    Initiate the ProblemView

--- src/components/sections/ValidationFormSection.vue
+++ src/components/sections/ValidationFormSection.vue
@@ -1,9 +1,83 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div class="image-side">
+      <div class="image-frame">
+        <img alt="Yahya Aqrom" class="image" src="/IntroductionSectionYahya.png" />
+      </div>
+    </div>
+    <div class="text-side">
+      <div class="text-salam">Assalamualaikum!</div>
+      <div class="text-intro">
+        Perkenalkan, nama saya Yahya Aqrom, seorang mahasiswa dari ITB yang tertarik dalam
+        mempelajari permasalahan kincir air tambak udang.
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  min-height: 400px;
+  width: 100%;
+  background-color: #1d3932;
+}
+.image-side {
+  display: flex;
+  flex: 50%;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  // border: 1px solid red;
+}
+.image-frame {
+  display: flex;
+  justify-content: center;
+  align-items: end;
+  aspect-ratio: 3 / 5;
+  width: 80%;
+  max-height: 300px;
+  max-width: 500px;
+  border-radius: 20px 20px 0 0;
+  background-color: #016241;
+  @media (max-width: 1024px) {
+    width: 100%;
+    height: 240px;
+  }
+}
+.image {
+  max-width: 100%;
+  max-height: 80%;
+
+  @media (max-width: 1024px) {
+    height: 225px;
+  }
+}
+.text-side {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 50%;
+  padding: 30px;
+  @media (max-width: 1024px) {
+    padding-top: 0;
+  }
+}
+.text-salam {
+  text-align: center;
+  font-weight: 700;
+  font-size: 2rem;
+  margin-bottom: 1rem;
+}
+.text-intro {
+  text-align: center;
+  font-size: 1rem;
+}
+</style>
--- src/views/ProblemView.vue
+++ src/views/ProblemView.vue
@@ -1,9 +1,41 @@
+<script setup>
+import IdentityFormSection from '../components/sections/IdentityFormSection.vue'
+import ValidationFormSection from '../components/sections/ValidationFormSection.vue'
+import TextExplanationSection from '../components/sections/TextExplanationSection.vue'
+import CTASection from '../components/sections/CTASection.vue'
+</script>
+
 <template>
-  <div>Problem View</div>
+  <IdentityFormSection></IdentityFormSection>
+  <ValidationFormSection></ValidationFormSection>
+  <ValidationFormSection></ValidationFormSection>
+  <ValidationFormSection></ValidationFormSection>
+  <TextExplanationSection>
+    <template v-slot:content>
+      Kematian mendadak pada kincir air bisa terjadi pada kincir mesin diesel maupun kincir listrik.
+    </template>
+  </TextExplanationSection>
+  <ValidationFormSection></ValidationFormSection>
+  <ValidationFormSection></ValidationFormSection>
+  <ValidationFormSection></ValidationFormSection>
+  <TextExplanationSection>
+    <template v-slot:content>
+      Masalah ini dapat dihindari, yaitu dengan mengawasi kincir sehingga penanganan dapat segera
+      dilakukan saat terjadi insiden kincir mati.
+    </template>
+  </TextExplanationSection>
+  <ValidationFormSection></ValidationFormSection>
+  <ValidationFormSection></ValidationFormSection>
+  <CTASection action="Kirim">
+    <template v-slot:text>
+      Untuk dapat menerima ide solusi dan undian pulsa Rp20.000, anda dapat mengisi nomor telepon di
+      bawah.
+    </template>
+  </CTASection>
 </template>
 
 <script>
-export default {}
+export default { components: { IdentityFormSection } }
 </script>
 
 <style lang="scss" scoped></style>
